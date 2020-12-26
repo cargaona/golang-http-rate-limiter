823377fdb29e0db884ba8380b52f60d3ba1a345c
Carlos Gaona <carlos.gaona@etermax.com>
2020-12-26 18:14:39 -0300

    Main file

--- go.mod
+++ go.mod
@@ -1,14 +1,5 @@
-module gitlab.com/personal/rateLimiter
+module github.com/cargaona/rateLimiter
 
 go 1.15
 
-require (
-	github.com/c2h5oh/datasize v0.0.0-20200825124411-48ed595a09d2 // indirect
-	github.com/influxdata/tdigest v0.0.1 // indirect
-	github.com/mailru/easyjson v0.7.6 // indirect
-	github.com/tsenart/go-tsz v0.0.0-20180814235614-0bd30b3df1c3 // indirect
-	github.com/tsenart/vegeta v12.7.0+incompatible // indirect
-	golang.org/x/net v0.0.0-20201209123823-ac852fbbde11 // indirect
-	golang.org/x/text v0.3.4 // indirect
-	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324
-)
+require golang.org/x/time v0.0.0-20201208040808-7e3f01d25324
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"golang.org/x/time/rate"
 	"log"
 	"net/http"
+	"sync"
 )
 
 var limiter = NewIPRateLimiter(490, 200)
@@ -18,7 +20,6 @@ func main() {
 
 func ping(responseWriter http.ResponseWriter, r *http.Request) {
 	responseWriter.Write([]byte("Finished"))
-//	fmt.Fprintf(responseWriter, "Finished!")
 }
 
 func limitMiddleware(mux http.Handler) http.Handler {
@@ -30,4 +31,52 @@ func limitMiddleware(mux http.Handler) http.Handler {
 		}
 		mux.ServeHTTP(responseWriter, request)
 	})
+}
+
+type IPRateLimiter struct {
+	ips         map[string]*rate.Limiter
+	mu          *sync.RWMutex
+	RateLimit   rate.Limit
+	TokenBucket int
+}
+
+func NewIPRateLimiter(rateLimit rate.Limit, tokenBucket int) *IPRateLimiter {
+	ipRateLimiter := &IPRateLimiter{
+		ips:         make(map[string]*rate.Limiter),
+		mu:          &sync.RWMutex{},
+		RateLimit:   rateLimit,
+		TokenBucket: tokenBucket,
+	}
+
+	return ipRateLimiter
+}
+
+func (i *IPRateLimiter) AddIP(ip string) *rate.Limiter {
+	//We need to lock the Struct because various requests may want to write/delete/read here at the same time.
+	i.mu.Lock()
+	// It's a Good practice to defer the unlock. There is a minimum penalty around 10 microseconds.
+	defer i.mu.Unlock()
+	// create a new limiter.
+	limiter := rate.NewLimiter(i.RateLimit, i.TokenBucket)
+	// Save the limiter in the hashmap for the IP associated.
+	i.ips[ip] = limiter
+
+	return limiter
+}
+
+func (i *IPRateLimiter) GetLimiter(ip string) *rate.Limiter {
+	i.mu.Lock()
+	//defer i.mu.Unlock()
+	limiter, limiterExists := i.ips[ip]
+	if !limiterExists {
+		// If limiter already exists for the given IP return the existing limiter.
+		i.mu.Unlock()
+		return i.AddIP(ip)
+	}
+	i.mu.Unlock()
+	return limiter
+}
+
+func (i *IPRateLimiter) GetNextAvailableTime(limiter *rate.Limiter){
+	limiter.Limit()
 }
